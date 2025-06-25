9c85b38f6815b2fc7404f63ee305e39faca7f20d
jejusamds <160725458+jejusamds@users.noreply.github.com>
2025-06-25 13:41:20 +0900

    관리자 등록 뷰 개선

--- Madmin/registration/reg_competition_view.php
+++ Madmin/registration/reg_competition_view.php
@@ -10,6 +10,29 @@ if (!$row) {
     error('잘못된 접근입니다.', 'competition_list.php');
     exit;
 }
+
+// 컬럼명 대신 보여줄 라벨 정의
+$labels = [
+    'f_applicant_type'   => '신청자구분',
+    'f_competition_idx'  => '대회구분',
+    'f_part'             => '참가부문',
+    'f_field'            => '종목분야',
+    'f_event'            => '참가종목',
+    'f_user_name'        => '이름',
+    'f_gender'           => '성별',
+    'f_user_name_en'     => '영문이름',
+    'f_birth_date'       => '생년월일',
+    'f_tel'              => '연락처',
+    'f_email'            => '이메일',
+    'f_zip'              => '우편번호',
+    'f_address1'         => '기본주소',
+    'f_address2'         => '상세주소',
+    'f_payer_name'       => '입금자명',
+    'f_payer_bank'       => '은행',
+    'f_payment_category' => '입금 구분',
+    'f_user_id'          => '회원ID',
+    'reg_date'           => '등록일',
+];
 ?>
 <div class="pageWrap">
     <div class="page-heading">
@@ -23,8 +46,10 @@ if (!$row) {
         <div class="panel">
             <table class="table noMargin" cellpadding="0" cellspacing="0">
                 <?php foreach ($row as $key => $val): ?>
+                    <?php if ($key === 'idx') continue; ?>
+                    <?php if (!isset($labels[$key])) continue; ?>
                     <tr>
-                        <td style="width:200px;"><?= htmlspecialchars($key, ENT_QUOTES) ?></td>
+                        <td style="width:200px;"><?= htmlspecialchars($labels[$key], ENT_QUOTES) ?></td>
                         <td><?= nl2br(htmlspecialchars($val, ENT_QUOTES)) ?></td>
                     </tr>
                 <?php endforeach; ?>
--- Madmin/registration/reg_edu_view.php
+++ Madmin/registration/reg_edu_view.php
@@ -10,6 +10,29 @@ if (!$row) {
     error('잘못된 접근입니다.', 'edu_list.php');
     exit;
 }
+
+// 컬럼명과 한글 라벨 매핑
+$labels = [
+    'f_type'            => '구분',
+    'f_news_idx'        => '교육구분',
+    'f_user_name'       => '이름/기관명',
+    'f_user_name_en'    => '영문이름/담당자',
+    'f_gender'          => '성별',
+    'f_birth_date'      => '생년월일',
+    'f_tel'             => '연락처',
+    'f_contact_phone'   => '담당자 연락처',
+    'f_zip'             => '우편번호',
+    'f_address1'        => '기본주소',
+    'f_address2'        => '상세주소',
+    'f_email'           => '이메일',
+    'f_issue_file'      => '파일',
+    'f_issue_file_name' => '파일명',
+    'f_payer_name'      => '입금자명',
+    'f_payer_bank'      => '은행',
+    'f_payment_category'=> '입금 구분',
+    'f_user_id'         => '회원ID',
+    'reg_date'          => '등록일',
+];
 ?>
 <div class="pageWrap">
     <div class="page-heading">
@@ -23,8 +46,12 @@ if (!$row) {
         <div class="panel">
             <table class="table noMargin" cellpadding="0" cellspacing="0">
                 <?php foreach ($row as $key => $val): ?>
+                    <?php if ($key === 'idx') continue; ?>
+                    <?php if (!isset($labels[$key])) continue; ?>
                     <tr>
-                        <td style="width:200px;"><?= htmlspecialchars($key, ENT_QUOTES) ?></td>
+                        <td style="width:200px;">
+                            <?= htmlspecialchars($labels[$key], ENT_QUOTES) ?>
+                        </td>
                         <td><?= nl2br(htmlspecialchars($val, ENT_QUOTES)) ?></td>
                     </tr>
                 <?php endforeach; ?>
